baf0cf40bad39cfe194fd0f2e520c5fc15e8be43
Matthew R <matthew.rawlings6@gmail.com>
2021-01-26 12:13:54 +0200

    removed navbar from main page

--- src/homepage.js
+++ src/homepage.js
@@ -5,7 +5,7 @@ import {loadMain} from './mainSection';
 export const loadHomePage = () => {
 
     const body = document.createElement("div");
-    body.appendChild(loadHeader());
+    // body.appendChild(loadHeader());
     body.appendChild(loadNavBar());
     body.appendChild(loadMain());
 
