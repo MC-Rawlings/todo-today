7685639472377ab5cc6e5acc3105068c1f832f6a
Matthew R <matthew.rawlings6@gmail.com>
2021-02-04 15:01:47 +0200

    added dropdown toggle and menu animations

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";!function(){const e=document.querySelector(".toggle-lists"),t=document.querySelector(".lists");e.addEventListener("click",(()=>{"none"===t.style.display?t.style.display="flex":t.style.display="none"}))}()})();
