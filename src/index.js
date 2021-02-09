bbced124bb82cdd7d95e0dd4c2f975c176158dd2
Matthew R <matthew.rawlings6@gmail.com>
2021-02-09 14:20:24 +0200

    styled modal and linked add-btn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import {toggleLists} from './dom';
 
+const addTaskBtn = document.querySelector(".tasks-add-btn");
+const modal = document.querySelector(".modal-bg");
+
+addTaskBtn.addEventListener("click", () => {
+    modal.style.display = "block";
+
+})
+
 toggleLists();
 
 
