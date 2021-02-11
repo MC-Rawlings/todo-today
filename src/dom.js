30ae4b98616ea7ae42d310c174089751445380f3
Matthew R <matthew.rawlings6@gmail.com>
2021-02-11 15:41:04 +0200

    added toggle function for tasks and lists

--- src/dom.js
+++ src/dom.js
@@ -1,12 +1,34 @@
 
 
-export function toggleLists() {
+export const toggleLists = (() => {
     const toggleBtn = document.querySelector(".toggle-lists");
     const lists = document.querySelector(".lists");
 
     toggleBtn.addEventListener("click", () => {
-        (lists.style.display === "none" ?
+        lists.style.display === "none" ?
         lists.style.display = "flex" :
-        lists.style.display = "none");
+        lists.style.display = "none";
+
     })
-}
+})();
+
+export const closeModal = (() => {
+    const cancelBtn = document.querySelector("#add-task__cancel");
+    const modal = document.querySelector(".modal-bg");
+
+    cancelBtn.addEventListener("click", () => {
+        modal.style.display = "none";
+    })
+
+})();
+
+export const openModal = (() => {
+    const addTaskBtn = document.querySelector(".tasks-add-btn");
+    const modal = document.querySelector(".modal-bg");
+
+    addTaskBtn.addEventListener("click", () => {
+    modal.style.display = "block";
+
+})
+
+})();
--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
 import {toggleLists} from './dom';
-
-const addTaskBtn = document.querySelector(".tasks-add-btn");
-const modal = document.querySelector(".modal-bg");
-
-addTaskBtn.addEventListener("click", () => {
-    modal.style.display = "block";
-
-})
-
-toggleLists();
-
-
+import {closeModal} from './dom';
 
